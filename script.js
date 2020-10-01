3d429f6756007375a18a4d6ba5ae36537d1d306a
VictoriaAde <adedayovicky123@gmail.com>
2020-10-01 13:16:11 +0100

    add dark and light mode

--- script.js
+++ script.js
@@ -6,13 +6,34 @@ function navMenu() {
 		menu.classList.remove('responsive');
 		document.querySelector('.menu_btn').style.display = 'inline-block';
 		document.querySelector('.close_btn').style.display = 'none';
-		document.querySelector('.menu_con').style.display = 'none';
+		// document.querySelector('.menu_con').style.visibility = 'hidden';
 	} else {
 		menu.classList.add('responsive');
-	    document.querySelector('.close_btn').style.display = 'inline-block';
+		document.querySelector('.close_btn').style.display = 'inline-block';
 		document.querySelector('.menu_btn').style.display = 'none';
-		document.querySelector('.menu_con').style.display = 'inline-block';
+		// document.querySelector('.menu_con').style.visibility = 'visible';
 
 	}
 }
+
+
+// // switching btw modes
+
+const sun = document.querySelector("#sun");
+const moon = document.querySelector("#moon");
+sun.addEventListener('click', function () {
+
+	document.documentElement.setAttribute('data-theme', 'light');
+
+})
+
+moon.addEventListener('click', function () {
+
+	document.documentElement.setAttribute('data-theme', 'dark');
+	
+})
+
+
+
+
 
