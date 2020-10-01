4f1b48ad061da6d8a221ad0a8ed392dff33f3088
VictoriaAde <adedayovicky123@gmail.com>
2020-10-01 23:15:59 +0100

    made minor fixes

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function navMenu() {
 		menu.classList.remove('responsive');
 		document.querySelector('.menu_btn').style.display = 'inline-block';
 		document.querySelector('.close_btn').style.display = 'none';
+		
 		// document.querySelector('.menu_con').style.visibility = 'hidden';
 	} else {
 		menu.classList.add('responsive');
@@ -24,15 +25,18 @@ const moon = document.querySelector("#moon");
 sun.addEventListener('click', function () {
 
 	document.documentElement.setAttribute('data-theme', 'light');
+	document.querySelector('.logo-dark').style.display = 'none';
+	document.querySelector('.logo-light').style.display = 'block';
 
 })
 
 moon.addEventListener('click', function () {
 
 	document.documentElement.setAttribute('data-theme', 'dark');
+	document.querySelector('.logo-dark').style.display = 'block';
+	document.querySelector('.logo-light').style.display = 'none';
 	
 })
-
 
 
 
