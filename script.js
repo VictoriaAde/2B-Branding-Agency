ea83eb6c6d6ef99de39d21f3bd8f1b070add9d75
VictoriaAde <adedayovicky123@gmail.com>
2020-10-03 10:34:55 +0100

    add new menu ico and fixed alignmentn

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function navMenu() {
 	var menu = document.querySelector('.menu_con');
 	if (menu.classList.contains('responsive')) {
 		menu.classList.remove('responsive');
-		document.querySelector('.menu_btn').style.display = 'inline-block';
-		document.querySelector('.close_btn').style.display = 'none';
-		
+		// document.querySelector('.menu_btn').style.display = 'inline-block';
+		// document.querySelector('.close_btn').style.display = 'none';
+
 		// document.querySelector('.menu_con').style.visibility = 'hidden';
 	} else {
 		menu.classList.add('responsive');
-		document.querySelector('.close_btn').style.display = 'inline-block';
-		document.querySelector('.menu_btn').style.display = 'none';
+		// document.querySelector('.close_btn').style.display = 'inline-block';
+		// document.querySelector('.menu_btn').style.display = 'none';
 		// document.querySelector('.menu_con').style.visibility = 'visible';
 
 	}
@@ -19,23 +19,23 @@ function navMenu() {
 
 
 // // switching btw modes
-
 const sun = document.querySelector("#sun");
 const moon = document.querySelector("#moon");
-sun.addEventListener('click', function () {
 
+sun.addEventListener('click', function () {
 	document.documentElement.setAttribute('data-theme', 'light');
 	document.querySelector('.logo-dark').style.display = 'none';
 	document.querySelector('.logo-light').style.display = 'block';
-
+	document.querySelector("#sun").style.display = 'none';
+	document.querySelector("#moon").style.display = 'block';
 })
 
 moon.addEventListener('click', function () {
-
 	document.documentElement.setAttribute('data-theme', 'dark');
 	document.querySelector('.logo-dark').style.display = 'block';
 	document.querySelector('.logo-light').style.display = 'none';
-	
+	document.querySelector("#moon").style.display = 'none';
+	document.querySelector("#sun").style.display = 'block';
 })
 
 
